1ef995867abf8fe7a9473c0d18b6093bbec77fe7
raphaelhudvoy <raphael.hudon-voyer@mail.mcgill.ca>
2013-11-13 12:23:27 -0500

    fb callback url

--- ouath.js
+++ ouath.js
@@ -2,7 +2,7 @@ var ids = {
 	fb: {
 	 	clientID: '253787698102458',
 	 	clientSecret: 'b82d714a1f5591a76ec7d74fc09d2f49',
-	 	callbackURL: 'http://127.0.0.1:4000/auth/facebook/callback'
+	 	callbackURL: 'http://speedtutoring-app.herokuapp.com/auth/facebook/callback'
 	}
 }
 

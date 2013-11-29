a88f85c1c63ca3f06b23954cef8780c140b0719d
raphaelhudvoy <raphael.hudon-voyer@mail.mcgill.ca>
2013-11-29 01:30:54 -0500

    socketManager missing

---
+++ speedTutoring/javascripts/services/socketManager.js
@@ -0,0 +1,35 @@
+Tuto.factory('WebSocketFactory', function ($rootScope, UserService) {
+    var socket = io.connect('/');
+
+    var Service = {};
+
+    Service.receive = function (event, cb) {
+        console.log('RECEIVES -> ', event);
+        socket.on(event, function () {
+            var args = arguments;
+            $rootScope.$apply(function () {
+                cb.apply(socket, args);
+            });
+        });
+    };
+
+    Service.emit = function (eventName, data, cb) {
+        console.log('EMIT -> ', eventName);
+        socket.emit(eventName, data, function () {
+            var args = arguments;
+            $rootScope.$apply(function () {
+                if (cb) {
+                    cb.apply(socket, args);
+                }
+            });
+        });
+    };
+
+    UserService.getCurrentUserId().then(function (userId){
+        socket.emit('join', userId);
+    }, function (err){
+        console.log(err);
+    });
+
+    return Service;
+});
